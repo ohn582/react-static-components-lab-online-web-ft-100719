98970753ef18494a701a5fb3cd0067e084ae8c48
Neal JIn Oh <no4259@gmail.com>
2020-02-22 00:53:35 +0000

    Done.

---
+++ src/EinsteinQuoteComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent';
+
+export default class EinsteinQuoteComponent extends Component {
+  render() {
+    return (
+      <div>
+        <GraceHopperQuoteComponent />
+      </div>
+    );
+  }
+}
